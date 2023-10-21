a29272befec2719952a19682b9f9ecc213360fd2
brodytrue <btm00005@mix.wvu.edu>
2023-10-21 13:31:36 -0400

    removed semicolons

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>CS 230L</h1>;
-      <h2>Section-003</h2>;
-      <p>WVU ID: 800351595</p>;
+      <h1>CS 230L</h1>
+      <h2>Section-003</h2>
+      <p>WVU ID: 800351595</p>
       <p>Hi, I am Brody</p>
     </div>
   );
